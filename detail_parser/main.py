a03274fe41bc81920daf1025bd4d38944d4295d5
cyh <tkionshao@gmail.com>
2018-07-10 20:35:58 +0800

    update

--- detail_parser/main.py
+++ detail_parser/main.py
@@ -1,9 +1,31 @@
 #!/usr/bin/python3
-from pageDetailParserClass import pageParser
+from pageDetailParserClass import pageParserCreator
+from datetime import datetime
+import pandas as pd
+import concurrent.futures
 
 if __name__ == '__main__':
+    df = pd.DataFrame(columns=['name','com','addr','loc','cnt','tl','othr'])
+    fus = []
+    i = 1
     with open(r'../urls_parser/data/urls_2018-07-09.csv') as f:
-        for i in range(10):
-            line = f.readline()
-            pageParser(line).printResult()
-    print(line)
+        with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+            while f.readline != '':
+                url = f.readline()
+                fus.append(executor.submit(pageParserCreator,url))
+                if i == 10:break
+
+            for future in concurrent.futures.as_completed(fus):
+                s = future.result()
+                df = df.append(s, ignore_index=True)
+            df.to_csv('./data/detail_().csv'.format(str(datetime.datetime.now().date())))
+
+    #format(str(datetime.datetime.now().date()))
+
+
+    #             s = pageParserCreator(url)
+    #             print('-' * 20)
+    #             df = df.append(s,ignore_index=True)
+    #             if i == 20:
+    #                 break
+    # print(df)
--- detail_parser/pageDetailParserClass.py
+++ detail_parser/pageDetailParserClass.py
@@ -2,48 +2,53 @@
 import requests
 import re
 from bs4 import BeautifulSoup as bs
+import pandas as pd
+from time import sleep
 
 class pageParser:
-    url = None
-    url = None
-    resp = None
-    soup = None
     def __init__(self,url):
         self.url = url
         self.resp = requests.get(url)
+        sleep(1)
         self.soup = bs(self.resp.text.replace('"',''))
 
     def get_name(self):
         name = self.soup.select('h1')[1].text.split('\n')[1].strip()
-        print(name,type(name))
+        print(name, type(name))
+        return name
 
     def get_company(self):
         company = self.soup.select('h1')[1].text.split('\n')[2]
-        print(company,type(company))
+        print(company, type(company))
+        return company
 
     def get_address(self):
         addr = self.soup.select('.addr')[0].text.split('\n')[1].strip()
-        print(addr,type(addr))
+        print(addr, type(addr))
+        return addr
 
     def get_location(self):
         loc = re.findall('var\ jlocation=\[(.+?),(.+?),',self.resp.text)[0]
-        print(loc,type(loc))
+        print(loc, type(loc))
+        return loc
 
     def get_content(self):
         cnt = self.soup.select_one("#job").find("div", {"class": 'content'}).p.text.strip().replace('\r','')
-        print(cnt)
+        print(cnt, type(cnt))
+        return cnt
 
     def get_tool(self):
         tl = self.soup.select('.tool')
         all_tl = ''
         for i in tl:
             all_tl += i.text + ' '
-        print(all_tl,type(all_tl))
+        print(all_tl, type(all_tl))
+        return all_tl
 
     def get_others(self):
-        othr = self.soup.select('.content')[1].select('dd')[7].text
-        print(othr,type(othr),len(othr))
-        print('-'*20)
+        othr = self.soup.select('.content')[1].select('dd')[7].text.replace('\r',' ')
+        print(othr, type(othr), len(othr))
+        return othr
 
     def printResult(self):
         self.get_name()
@@ -54,17 +59,31 @@ class pageParser:
         self.get_tool()
         self.get_others()
 
+def pageParserCreator(url):
+    Page = pageParser(url) # init Page
+
+    name = Page.get_name()
+    com = Page.get_company()
+    addr = Page.get_address()
+    loc = Page.get_location()
+    cnt = Page.get_content()
+    tl = Page.get_tool()
+    othr = Page.get_others()
+    s = pd.Series([name,com,addr,loc,cnt,tl,othr],index=['name','com','addr','loc','cnt','tl','othr'])
+    return s
+
 if __name__ == '__main__':
     urls = ['https://www.104.com.tw/job/?jobno=695ve&jobsource=pda',
             'http://www.104.com.tw/job/?jobno=547dz&amp;jobsource=n104bank1',
             'http://www.104.com.tw/job/?jobno=5snph&amp;jobsource=n104bank1',
-            'https://www.104.com.tw/job/?jobno=5v3r1&amp;jobsource=n104bank1']
+            'https://www.104.com.tw/job/?jobno=5v3r1&amp;jobsource=n104bank1',
+            'http://www.104.com.tw/job/?jobno=6543k&amp;jobsource=n104bank1']
+
+    df = pd.DataFrame(columns=['name','com','addr','loc','cnt','tl','othr'])
     for url in urls:
-        Page = pagePaser(url)
-        Page.get_name()
-        Page.get_company()
-        Page.get_address()
-        Page.get_location()
-        Page.get_content()
-        Page.get_tool()
-        Page.get_others()
+        print('-' * 20)
+        s = pageParserCreator(url)
+        print('-' * 20)
+        print(s)
+        df = df.append(s,ignore_index=True)
+    print(df.head())
